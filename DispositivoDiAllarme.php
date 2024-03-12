9cd9cd26bd480a589eed6fd0627fb470d41a8372
lai2-24 <lai2-24@itismeucci.com>
2024-03-12 12:35:37 +0100

    Creata classe DispositivoDiAllarme

---
+++ DispositivoDiAllarme.php
@@ -0,0 +1,36 @@
+<?php
+    class DispositivoDiAllarme implements JsonSerializable{
+        protected $identificativo;
+        protected $numero_di_telefono;
+
+        public function __construct($id, $num){
+            $this->identificativo = $id;
+            $this->numero_di_telefono = $num;
+        }
+
+        public function getIdentificativo(){
+            return $this->identificativo;
+        }
+
+        public function getNumeroDiTelefono(){
+            return $this->numero_di_telefono;
+        }
+
+        public function setNumeroDiTelefono($numero_di_telefono){
+            $this->numero_di_telefono = $numero_di_telefono
+        }
+
+        public function setIdentificativo($identificativo){
+            $this->identificativo = $identificativo
+        }
+
+        public function jsonSerialize(){
+            $ritorno = [
+                "identificativo" => $this->identificativo,
+                "numero_di_telefono" => $this->numero_di_telefono
+            ]
+
+            return $ritorno;
+        }
+    }
+?>
